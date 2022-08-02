7d3fb12cf4fdff527151e7d90854002042cb3688
Clément Joye <clement.joye@authority.se>
2022-08-02 22:51:40 +0200

    Added Allow CORS to query Umami for 200

--- next.config.js
+++ next.config.js
@@ -33,6 +33,16 @@ module.exports = {
           },
         ],
       },
+      {
+        // mathching dashboard route
+        source: "/dashboard",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      },
     ];
   },
 };
